508ec14d86fc81cdc442152d9fa811cd3c539a66
SurajRampatwar <surajrampatwar123@gmail.com>
2022-05-26 19:27:21 +0530

    Welcome to Address Book Program

---
+++ src/AddressBookSystem/AddressBook.java
@@ -0,0 +1,11 @@
+package AddressBookSystem;
+import java.util.Scanner;
+public class AddressBook {
+    public static void start() {
+        System.out.println("Welcome to Address Book Program");
+    }
+    public static void main(String[] args) {
+        start();
+    }
+}
+
