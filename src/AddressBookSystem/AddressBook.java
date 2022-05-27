fc2eaf9273caa2a5910b969088f25a6998f1e2e5
SurajRampatwar <surajrampatwar123@gmail.com>
2022-05-27 17:32:32 +0530

    Ability to create contct

--- src/AddressBookSystem/AddressBook.java
+++ src/AddressBookSystem/AddressBook.java
@@ -1,11 +1,20 @@
 package AddressBookSystem;
 import java.util.Scanner;
 public class AddressBook {
-    public static void start() {
-        System.out.println("Welcome to Address Book Program");
-    }
     public static void main(String[] args) {
-        start();
+        System.out.println("Welcome to AddressBook Program");
+        Person contact = new Person();
+        while (true) {
+            System.out.println("[1] Add New Contact");
+            System.out.println("[2] Edit Existing Contact");
+            System.out.println("[3] Delete Contact");
+            System.out.println("[4] Exit");
+            Scanner scan = new Scanner(System.in);
+            int num = scan.nextInt();
+            if (num == 1) {
+                contact.AddContact();
+                break;
+            }
+        }
     }
 }
-
